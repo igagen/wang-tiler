0b600a1943e2740a71114c213a02096f7ccd5ac1
Ingmar Gagen <igagen@gmail.com>
2011-06-20 01:36:37 -0700

    FINISHED: Calculate color difference between two pixels

--- js/views/wang-view.js
+++ js/views/wang-view.js
@@ -24,8 +24,8 @@ var WangView = Backbone.View.extend({
     for (var i = 0; i < this.TILES.length; i++) {
       var tile = this.TILES[i];
       this.tiles[tile] = {};
-      this.tiles[tile].canvas = this[tile + 'Canvas'] = $("#wang-tiles ." + tile);
-      this.tiles[tile].context = this[tile + 'Context'] = this[tile + 'Canvas'][0].getContext("2d");
+      this.tiles[tile].canvas = this[tile + 'DiamondTileCanvas'] = $("#diamond-tiles ." + tile);
+      this.tiles[tile].context = this[tile + 'DiamondTileContext'] = this[tile + 'DiamondTileCanvas'][0].getContext("2d");
       this.tiles[tile].canvas = this[tile + 'SubSampleCanvas'] = $("#sub-samples ." + tile);
       this.tiles[tile].context = this[tile + 'SubSampleContext'] = this[tile + 'SubSampleCanvas'][0].getContext("2d");
     }
@@ -36,7 +36,7 @@ var WangView = Backbone.View.extend({
     this.imageUploadButton = $("#image-upload-button");
 
     var blockSize = this.BLOCK_SIZE;
-    $("#wang-tiles canvas, #sub-samples canvas, #scratch").each(function() {
+    $("#diamond-tiles canvas, #sub-samples canvas, #wang-tiles canvas, #scratch").each(function() {
       $(this).attr('width', blockSize);
       $(this).attr('height', blockSize);
     })
@@ -86,25 +86,34 @@ var WangView = Backbone.View.extend({
   },
 
   handleMouseUp: function(event) {
-    this.sampleRect = this.getSelectionRect(event);
-    this.generateDiamonds();
-    this.generateDiamondTiles();
-    this.generateSubSamples();
+    var r = this.getSelectionRect(event);
+    if (r.width >= this.BLOCK_SIZE && r.height >= this.BLOCK_SIZE) {
+      this.sampleRect = r;
+      this.drawSampleRect(this.sampleRect);
+      this.generateDiamonds();
+      this.generateDiamondTiles();
+      this.generateSubSamples();
+    }
+    else {
+      this.sourceContext.drawImage(this.sourceImage, 0, 0);
+    }
+
     this.sampling = false;
   },
 
   handleMouseMove: function(event) {
-    if (this.sampling) {
-      var rect = this.getSelectionRect(event);
+    if (this.sampling) this.drawSampleRect(this.getSelectionRect(event));
+  },
 
-      this.sourceContext.drawImage(this.sourceImage, 0, 0);
+  drawSampleRect: function(rect) {
+    this.sourceContext.drawImage(this.sourceImage, 0, 0);
 
-      this.sourceContext.fillStyle = 'rgba(255,255,255,0.25)';
-      this.sourceContext.fillRect(rect.x, rect.y, rect.width, rect.height);
+    this.sourceContext.fillStyle = 'rgba(255,255,255,0.25)';
+    this.sourceContext.fillRect(rect.x, rect.y, rect.width, rect.height);
 
-      this.sourceContext.strokeStyle = 'rgba(0,0,0,1)';
-      this.sourceContext.strokeRect(rect.x, rect.y, rect.width, rect.height);
-    }
+    this.sourceContext.strokeStyle = 'rgba(0,0,0,1)';
+    this.sourceContext.lineWidth = 2;
+    this.sourceContext.strokeRect(rect.x, rect.y, rect.width, rect.height);
   },
 
   generateDiamonds: function() {
@@ -177,7 +186,7 @@ var WangView = Backbone.View.extend({
 
       // Draw sub-sample rectangles on original source canvas to indicate their source location
       var c = this.sourceContext;
-      c.lineWidth = 2;
+      c.lineWidth = 3;
       c.strokeStyle = color;
 
       c.beginPath();
@@ -241,7 +250,7 @@ var WangView = Backbone.View.extend({
     this.scratchContext.clearRect(0, 0, this.BLOCK_SIZE, this.BLOCK_SIZE);
     for (var t = 0; t < this.TILES.length; t++) {
       var tile = this.TILES[t];
-      var context = this[tile + 'Context'];
+      var context = this[tile + 'DiamondTileContext'];
       context.clearRect(0, 0, this.BLOCK_SIZE, this.BLOCK_SIZE);
     }
 
@@ -250,7 +259,7 @@ var WangView = Backbone.View.extend({
 
       for (var t = 0; t < this.TILES.length; t++) {
         var tile = this.TILES[t];
-        var context = this[tile + 'Context'];
+        var context = this[tile + 'DiamondTileContext'];
         context.globalCompositeOperation = 'lighter';
 
         if (tile[0] == color) this.drawTopTriangle(context, color);
@@ -323,6 +332,61 @@ var WangView = Backbone.View.extend({
     context.lineTo(0, this.BLOCK_SIZE);
     context.lineTo(this.BLOCK_SIZE / 2, this.BLOCK_SIZE / 2);
     context.fill();
+  },
+
+  rgbToXyz: function(r, g, b) {
+    r = r / 255;
+    g = g / 255;
+    b = b / 255;
+
+    if (r > 0.04045) r = Math.pow(((r + 0.055) / 1.055), 2.4);
+    else r = r / 12.92;
+    if (g > 0.04045) g = Math.pow(((g + 0.055) / 1.055), 2.4);
+    else g = g / 12.92;
+    if (b > 0.04045) b = Math.pow(((b + 0.055) / 1.055), 2.4);
+    else b = b / 12.92;
+
+    r = r * 100;
+    g = g * 100;
+    b = b * 100;
+
+    return [
+      r * 0.4124 + g * 0.3576 + b * 0.1805,
+      r * 0.2126 + g * 0.7152 + b * 0.0722,
+      r * 0.0193 + g * 0.1192 + b * 0.9505
+    ];
+  },
+
+  xyzToLab: function(x, y, z) {
+    // Constants
+    var v1 = 1 / 3;
+    var v2 = 16 / 116;
+
+    x = x / 95.047;
+    y = y / 100;
+    z = z / 108.883;
+
+    if (x > 0.008856) x = Math.pow(x, v1);
+    else x = (7.787 * x) + v2;
+    if (y > 0.008856) y = Math.pow(y, v1);
+    else y = (7.787 * y) + v2;
+    if (z > 0.008856) z = Math.pow(y, v1);
+    else z = (7.787 * z) + v2;
+
+    return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)];
+  },
+
+  rgbToLab: function(r, g, b) {
+    var xyz = this.rgbToXyz(r, g, b);
+    return xyzToLab(xyz[0], xyz[1], xyz[2]);
+  },
+
+  colorDifference: function(c1, c2) {
+    // e76 algorithm is simply the Euclidean distance between the two colors in the Lab color space
+    var dL = c2[0] - c1[0];
+    var da = c2[1] - c1[1];
+    var db = c2[2] - c1[2];
+    return Math.sqrt(dL * dL + da * da + db * db);
   }
 });
 
